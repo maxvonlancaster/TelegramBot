9ee5f92a78de11606c487e9f93aa2f4129ab0a5c
maxvonlancaster <38358502+maxvonlancaster@users.noreply.github.com>
2019-09-04 16:35:00 +0300

    Add project files.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        private static IContainer Container { get; set; }
+
+        static void Main(string[] args)
+        {
+            ConfigureServices();
+        }
+
+        public static void ConfigureServices()
+        {
+            var builder = new ContainerBuilder();
+
+            // Configure services
+            Container = builder.Build();
+        }
+    }
+}
---
+++ TelegramBot/Services/MessagingService.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+
+namespace TelegramBot.Services
+{
+    public class MessagingService : IStartable
+    {
+        private static readonly TelegramBotClient Bot = 
+            new TelegramBotClient("955563107:AAFBR8hVPedVsmCAy9JJ4C_DZTlzbDxjJzQ");
+
+        public void Start()
+        {
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.StartReceiving();
+        }
+
+        private void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                Console.WriteLine(e.Message.Text);
+            }
+        }
+    }
+}
